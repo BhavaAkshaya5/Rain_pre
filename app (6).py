2c743e771fd0d58f9a01715fb91747c969be72f3
BhavaAkshaya5 <bhavaakshaya2006@gmail.com>
2025-06-19 20:50:23 +0530

    Update app (6).py

--- app (6).py
+++ app (6).py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1trbuaUr7mmFk-GAMQBkOy2brj0LGDHnH
 """
 
-!pip install streamlit
-
 import streamlit as st
 import requests
 from datetime import datetime
